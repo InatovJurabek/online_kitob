8fe3b058e4a51c39fdfe8a53d53fc901a5d9eb3d
InatovJurabek <inatovjorabek10@gmail.com>
2025-10-07 21:55:29 +0500

    Delete apps directory

--- apps/book/api_endpoints/books/book_list/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import generics
-from apps.book.models import Books
-from .serializers import BookListSerializer
-
-class BookListView(generics.ListAPIView):
-    queryset = Books.objects.all()
-    serializer_class = BookListSerializer
-    
--- apps/book/api_endpoints/user/user_detail/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from apps.book.models import CustomUser
-
-
-class CustomUserDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'phone_number', 'address']
-        
